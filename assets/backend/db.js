f7f5acf6484b9a9ba770f80f27d4eb5a63c737b0
Migeku64 <migeku64@gmail.com>
2025-03-04 11:50:50 -0500

    Add files via upload

---
+++ assets/backend/db.js
@@ -0,0 +1,18 @@
+import pg from "pg"
+
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect()
+  .then(() => console.log('🟢 Conectado a PostgreSQL'))
+  .catch(err => console.error('🔴 Error al conectar a PostgreSQL:', err));
+
+module.exports = pool;
---
+++ assets/backend/key.js
@@ -0,0 +1,8 @@
+module.exports={
+    database:{
+        host: '127.0.0.1',
+        user: 'Testt',
+        password: '123',
+        database: 'LibreriaBD'
+    },
+}
